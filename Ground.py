82886ea0e96c00fe9202190d4796cdf78fe22b7f
Robertwwp <wwpliv@gmail.com>
2018-02-25 16:19:10 -0500

    with new function find correspondences

--- Ground.py
+++ Ground.py
@@ -60,6 +60,42 @@ def OPflow(previmg):
         cam.release()
     return flow
 
+#find the correspondening point
+def find_corr(y,x,prev):
+    bp,gp,rp = cv2.split(prev)
+
+    #change to robot's camera motion later
+    cap=input('press 1 if the camera is oriented properly')
+    if cap==1:
+        cam = cv2.VideoCapture(1)
+        ret, img = cam.read()
+        b,g,r = cv2.split(img)
+
+        for index in range(len(y)):
+            pix_b=float(bp[y[index]][x[index]])
+            pix_g=float(gp[y[index]][x[index]])
+            pix_r=float(rp[y[index]][x[index]])
+
+            corr=np.zeros(shape=(32,32))
+            corr_sum_i=0
+            corr_sum_j=0
+            corr_count=0
+            for i in range(32):
+                for j in range(32):
+                    #turn the camera to the right
+                    corr[i][j]=abs(float(b[y[index]+i][x[index]-j])-pix_b)+abs(float(g[y[index]+i][x[index]-j])-pix_g)+abs(float(r[y[index]+i][x[index]-j])-pix_r)
+                    if corr[i][j]<10:
+                            corr_sum_i=corr_sum_i+i
+                            corr_sum_j=corr_sum_j+j
+                            corr_count=corr_count+1
+            corr_i=y[index]+int(corr_sum_i/corr_count)
+            corr_j=x[index]-int(corr_sum_j/corr_count)
+            cv2.circle(img, (corr_j, corr_i), 1, (0, 255, 0), 2)
+        return corr,img
+    else:
+        return None
+
+
 '''homography induced similarity'''
 #def Homo():
 '''find corespondences/ for each region? for edges? for the whole image?'''
@@ -76,7 +112,10 @@ while i<1:
 
     bri=std_bri(img,step,h,w,y,x)
     color_std,color_avg=color(img,step,h,w,y,x)
-    #cam.release()
+    cam.release()
+    y_c=[240,240,272,272]
+    x_c=[320,352,320,352]
+    corr, img=find_corr(y_c,x_c,img)
     #flow=OPflow(img)
 
     #pixels in the lower middle part of the image represent the ground
