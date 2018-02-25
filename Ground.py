dd7ea1dce5f4223c60e6519f5eeda1a87a275e90
Robertwwp <wwpliv@gmail.com>
2018-02-25 14:48:32 -0500

    #3
    leave optical flow away fro now

--- Ground.py
+++ Ground.py
@@ -52,13 +52,17 @@ def OPflow(previmg):
     prevgray = cv2.cvtColor(previmg, cv2.COLOR_BGR2GRAY)
     cap=input('press 1 if the camera is oriented properly')
     if cap==1:
+        cam = cv2.VideoCapture(1)
         ret, img = cam.read()
+        cv2.imshow('op',img)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        cam.release()
     return flow
 
 '''homography induced similarity'''
-def Homo():
+#def Homo():
+'''find corespondences/ for each region? for edges? for the whole image?'''
 
 
 
@@ -72,7 +76,8 @@ while i<1:
 
     bri=std_bri(img,step,h,w,y,x)
     color_std,color_avg=color(img,step,h,w,y,x)
-    flow=OPflow(img)
+    #cam.release()
+    #flow=OPflow(img)
 
     #pixels in the lower middle part of the image represent the ground
     g_cri_bri=(bri[len(y)-1][len(x)/2-1]+bri[len(y)-1][len(x)/2-2]+bri[len(y)-1][len(x)/2]+
@@ -81,15 +86,16 @@ while i<1:
                 color_std[len(y)-2][len(x)/2-1]+color_std[len(y)-2][len(x)/2-2]+color_std[len(y)-2][len(x)/2])/6
     g_cri_cavg=(color_avg[len(y)-1][len(x)/2-1]+color_avg[len(y)-1][len(x)/2-2]+color_avg[len(y)-1][len(x)/2]+
                 color_avg[len(y)-2][len(x)/2-1]+color_avg[len(y)-2][len(x)/2-2]+color_avg[len(y)-2][len(x)/2])/6
-    g_cri_flow=(flow[len(y)-1][len(x)/2-1][0]+flow[len(y)-1][len(x)/2-2][0]+flow[len(y)-1][len(x)/2][0]+
+    '''g_cri_flow=(flow[len(y)-1][len(x)/2-1][0]+flow[len(y)-1][len(x)/2-2][0]+flow[len(y)-1][len(x)/2][0]+
                 flow[len(y)-2][len(x)/2-1][0]+flow[len(y)-2][len(x)/2-2][0]+flow[len(y)-2][len(x)/2][0])/6
-    print(g_cri_flow)
+    print(g_cri_flow)'''
 
     #compare, decide and draw the ground regions
     for i in range(len(y)):
         for j in range(len(x)):
-            '''weighted method...'''
-            if abs(bri[i][j]-g_cri_bri)<40 and abs(color_std[i][j]-g_cri_cstd)<30 and abs(color_avg[i][j]-g_cri_cavg)<50 and abs(flow[i][j][0]-g_cri_flow)<0.8:
+            '''weighted method...optical flow has less impact for the pixels far away,can be used just for simple test'''
+            if abs(bri[i][j]-g_cri_bri) + abs(color_std[i][j]-g_cri_cstd) +abs(color_avg[i][j]-g_cri_cavg)<80:
+                #and abs(flow[i][j][0]-g_cri_flow)<0.8:
             #if color_flag[i][j]==1 and bri_flag[i][j]==1:
                 img = cv2.rectangle(img,(x[j]-step/2,y[i]-step/2),(x[j]+step/2,y[i]+step/2),(0,0,255),3)
     cv2.imshow('image',img)
@@ -97,5 +103,5 @@ while i<1:
     if cv2.waitKey(0) & 0xFF == ord('q'):
         break
 
-cam.release()
+#cam.release()
 cv2.destroyAllWindows()
